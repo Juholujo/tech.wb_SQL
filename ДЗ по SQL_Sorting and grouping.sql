cbb5765544a8c7f22415f3ef2200b9fd4f75bab6
Juholujo <goshaperelygin@gmail.com>
2024-11-06 20:13:23 +0300

    изменение

--- ДЗ по SQL_Sorting and grouping.sql
+++ ДЗ по SQL_Sorting and grouping.sql
@@ -30,23 +30,50 @@ GROUP BY
 
 -- 2 Часть
 -- Пункт 1
-
 SELECT
     seller_id,
     COUNT(DISTINCT category) AS total_categ,
+    AVG(rating) AS avg_rating,
     SUM(revenue) AS total_revenue,
-    ROUND(AVG(rating), 2) AS avg_rating,
     CASE
-        WHEN COUNT(DISTINCT category) > 1 AND SUM(revenue) > 50000 THEN 'rich'
-        WHEN COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000 THEN 'poor'
+        WHEN SUM(revenue) > 50000 THEN 'rich'
+        ELSE 'poor'
     END AS seller_type
 FROM
     sellers
 WHERE
-    category != 'Bedding'  -- Исключаем категорию "Bedding"
+    category != 'Bedding'
 GROUP BY
     seller_id
 HAVING
-    COUNT(DISTINCT category) > 1  -- Оставляем только тех, кто продает более одной категории товаров
+    COUNT(DISTINCT category) > 1
+ORDER BY
+    seller_id ASC;
+
+-- 2 Часть
+-- Пункт 2
+SELECT
+    seller_id,
+    FLOOR((CURRENT_DATE - TO_DATE(MIN(date_reg), 'DD/MM/YYYY')) / 30)::int AS month_from_registration,
+    (SELECT MAX(delivery_days) - MIN(delivery_days)
+     FROM sellers
+     WHERE category != 'Bedding'
+       AND seller_id IN (
+           SELECT seller_id
+           FROM sellers
+           WHERE category != 'Bedding'
+           GROUP BY seller_id
+           HAVING COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000
+       )
+    ) AS max_delivery_difference
+FROM
+    sellers
+WHERE
+    category != 'Bedding'
+GROUP BY
+    seller_id
+HAVING
+    COUNT(DISTINCT category) > 1
+    AND SUM(revenue) <= 50000
 ORDER BY
     seller_id ASC;
