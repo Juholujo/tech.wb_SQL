cb2dd3c04b699787994ffdec6a655e2fc722193c
Juholujo <goshaperelygin@gmail.com>
2024-11-06 19:47:17 +0300

    и

--- ДЗ по SQL_Sorting and grouping.sql
+++ ДЗ по SQL_Sorting and grouping.sql
@@ -27,3 +27,26 @@ WHERE
     name ILIKE '%hair%' OR name ILIKE '%home%'  -- Используем ILIKE для поиска без учета регистра
 GROUP BY
     category;
+
+-- 2 Часть
+-- Пункт 1
+
+SELECT
+    seller_id,
+    COUNT(DISTINCT category) AS total_categ,
+    SUM(revenue) AS total_revenue,
+    ROUND(AVG(rating), 2) AS avg_rating,
+    CASE
+        WHEN COUNT(DISTINCT category) > 1 AND SUM(revenue) > 50000 THEN 'rich'
+        WHEN COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000 THEN 'poor'
+    END AS seller_type
+FROM
+    sellers
+WHERE
+    category != 'Bedding'  -- Исключаем категорию "Bedding"
+GROUP BY
+    seller_id
+HAVING
+    COUNT(DISTINCT category) > 1  -- Оставляем только тех, кто продает более одной категории товаров
+ORDER BY
+    seller_id ASC;
