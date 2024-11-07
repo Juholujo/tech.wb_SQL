765762aff75ab414746eb8097446e75cda48e394
Juholujo <goshaperelygin@gmail.com>
2024-11-07 18:59:14 +0300

    new

--- ДЗ по SQL_Window functions.sql
+++ ДЗ по SQL_Window functions.sql
@@ -239,3 +239,80 @@ FROM
     ranked_sales
 WHERE
     prev_sales IS NOT NULL;
+
+--- Часть 3
+CREATE TABLE query (
+    searchid SERIAL PRIMARY KEY,
+    year INT,
+    month INT,
+    day INT,
+    userid INT,
+    ts BIGINT,        
+    devicetype VARCHAR(50),
+    deviceid VARCHAR(50),
+    query TEXT
+);
+
+
+INSERT INTO query (year, month, day, userid, ts, devicetype, deviceid, query)
+VALUES
+(2023, 1, 1, 1, 1672531200, 'android', 'device_1', 'к'),
+(2023, 1, 1, 1, 1672531210, 'android', 'device_1', 'ку'),
+(2023, 1, 1, 1, 1672531220, 'android', 'device_1', 'куп'),
+(2023, 1, 1, 1, 1672531230, 'android', 'device_1', 'купи'),
+(2023, 1, 1, 1, 1672531240, 'android', 'device_1', 'купит'),
+(2023, 1, 1, 1, 1672531250, 'android', 'device_1', 'купить'),
+(2023, 1, 1, 1, 1672531400, 'android', 'device_1', 'купить куртку'),
+(2023, 1, 1, 1, 1672533600, 'android', 'device_1', 'ноутбук'),
+(2023, 1, 1, 2, 1672531300, 'android', 'device_2', 'тел'),
+(2023, 1, 1, 2, 1672531365, 'android', 'device_2', 'телефон'),
+(2023, 1, 1, 2, 1672531900, 'android', 'device_2', 'тел'),
+(2023, 1, 1, 3, 1672531500, 'ios', 'device_3', 'планшет'),
+(2023, 1, 1, 3, 1672531520, 'ios', 'device_3', 'планшеты apple'),
+(2023, 1, 2, 1, 1672617600, 'android', 'device_1', 'телевизор'),
+(2023, 1, 2, 2, 1672617700, 'android', 'device_2', 'фотоаппарат');
+
+
+WITH query_with_next AS (
+    SELECT
+        q.*,
+        LEAD(q.ts) OVER (PARTITION BY q.userid, q.deviceid ORDER BY q.ts) AS next_ts,
+        LEAD(q.query) OVER (PARTITION BY q.userid, q.deviceid ORDER BY q.ts) AS next_query
+    FROM
+        query q
+),
+is_final_calculated AS (
+    SELECT
+        *,
+        CASE
+
+            WHEN next_ts IS NULL THEN 1
+
+            WHEN (next_ts - ts) > 180 THEN 1
+
+            WHEN (next_ts - ts) > 60 AND LENGTH(next_query) < LENGTH(query) THEN 2
+            ELSE 0
+        END AS is_final_value
+    FROM
+        query_with_next
+)
+SELECT
+    year,
+    month,
+    day,
+    userid,
+    ts,
+    devicetype,
+    deviceid,
+    query,
+    next_query,
+    is_final_value AS is_final
+FROM
+    is_final_calculated
+WHERE
+
+    date_trunc('day', to_timestamp(ts)) = '2023-01-01'::date
+    AND devicetype = 'android'
+    AND is_final_value IN (1, 2)
+ORDER BY
+    userid, deviceid, ts;
