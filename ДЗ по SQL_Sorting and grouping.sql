5f19e77a1d138c3be210fe92c204e54a76d904be
Juholujo <goshaperelygin@gmail.com>
2024-11-08 22:52:55 +0300

    ~

--- ДЗ по SQL_Sorting and grouping.sql
+++ ДЗ по SQL_Sorting and grouping.sql
@@ -1,60 +1,40 @@
 -- 1 Часть
 -- Пункт 1
-SELECT
-    city,
+SELECT city,
     CASE
         WHEN age BETWEEN 0 AND 20 THEN 'young'
         WHEN age BETWEEN 21 AND 49 THEN 'adult'
         ELSE 'old'
     END AS age_category,
     COUNT(id) AS user_count
-FROM
-    users
-GROUP BY
-    city,
-    age_category
-ORDER BY
-    user_count desc;
+FROM users
+GROUP BY city, age_category
+ORDER BY user_count desc;
 
 -- 1 Часть
 -- Пункт 2
-    SELECT
-    category,
-    ROUND(AVG(price), 2) AS avg_price
-FROM
-    products
+SELECT category, ROUND(AVG(price), 2) AS avg_price
+FROM products
 WHERE
-    name ILIKE '%hair%' OR name ILIKE '%home%'  -- Используем ILIKE для поиска без учета регистра
-GROUP BY
-    category;
+    name ILIKE '%hair%' OR name ILIKE '%home%'
+GROUP BY category;
 
 -- 2 Часть
 -- Пункт 1
-SELECT
-    seller_id,
-    COUNT(DISTINCT category) AS total_categ,
-    AVG(rating) AS avg_rating,
-    SUM(revenue) AS total_revenue,
+SELECT seller_id, COUNT(DISTINCT category) AS total_categ, AVG(rating) AS avg_rating, SUM(revenue) AS total_revenue,
     CASE
         WHEN SUM(revenue) > 50000 THEN 'rich'
         ELSE 'poor'
     END AS seller_type
-FROM
-    sellers
-WHERE
-    category != 'Bedding'
-GROUP BY
-    seller_id
-HAVING
-    COUNT(DISTINCT category) > 1
-ORDER BY
-    seller_id ASC;
+FROM sellers
+WHERE category != 'Bedding'
+GROUP BY seller_id
+HAVING COUNT(DISTINCT category) > 1
+ORDER BY seller_id ASC;
 
 -- 2 Часть
 -- Пункт 2
-SELECT
-    seller_id,
-    FLOOR((CURRENT_DATE - TO_DATE(MIN(date_reg), 'DD/MM/YYYY')) / 30)::int AS month_from_registration,
+SELECT seller_id, FLOOR((CURRENT_DATE - TO_DATE(MIN(date_reg), 'DD/MM/YYYY')) / 30)::int AS month_from_registration,
     (SELECT MAX(delivery_days) - MIN(delivery_days)
      FROM sellers
      WHERE category != 'Bedding'
@@ -66,31 +46,17 @@ SELECT
            HAVING COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000
        )
     ) AS max_delivery_difference
-FROM
-    sellers
-WHERE
-    category != 'Bedding'
-GROUP BY
-    seller_id
-HAVING
-    COUNT(DISTINCT category) > 1
-    AND SUM(revenue) <= 50000
-ORDER BY
-    seller_id ASC;
+FROM sellers
+WHERE category != 'Bedding'
+GROUP BY seller_id
+HAVING COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000
+ORDER BY seller_id ASC;
 
 -- 2 Часть
 -- Пункт 3
-SELECT
-    seller_id,
-    string_agg(DISTINCT category, ' - ' ORDER BY category) AS category_pair
-FROM
-    sellers
-WHERE
-    EXTRACT(YEAR FROM TO_DATE(date_reg, 'DD/MM/YYYY')) = 2022
-GROUP BY
-    seller_id
-HAVING
-    COUNT(DISTINCT category) = 2
-    AND SUM(revenue) > 75000
-ORDER BY
-    seller_id ASC;
+SELECT seller_id, string_agg(DISTINCT category, ' - ' ORDER BY category) AS category_pair
+FROM sellers
+WHERE EXTRACT(YEAR FROM TO_DATE(date_reg, 'DD/MM/YYYY')) = 2022
+GROUP BY seller_id
+HAVING COUNT(DISTINCT category) = 2 AND SUM(revenue) > 75000
+ORDER BY seller_id ASC;
