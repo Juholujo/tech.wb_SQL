c0756aadf18c513130bc569e3a5005648af2afd7
Juholujo <goshaperelygin@gmail.com>
2024-11-09 01:07:48 +0300

    РО

--- ДЗ по SQL_Sorting and grouping.sql
+++ ДЗ по SQL_Sorting and grouping.sql
@@ -34,23 +34,25 @@ ORDER BY seller_id ASC;
 
 -- 2 Часть
 -- Пункт 2
-SELECT seller_id, FLOOR((CURRENT_DATE - TO_DATE(MIN(date_reg), 'DD/MM/YYYY')) / 30)::int AS month_from_registration,
-    (SELECT MAX(delivery_days) - MIN(delivery_days)
-     FROM sellers
-     WHERE category != 'Bedding'
-       AND seller_id IN (
-           SELECT seller_id
-           FROM sellers
-           WHERE category != 'Bedding'
-           GROUP BY seller_id
-           HAVING COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000
-       )
-    ) AS max_delivery_difference
+SELECT seller_id,
+       FLOOR((CURRENT_DATE - MIN(TO_DATE(date_reg, 'DD/MM/YYYY'))) / 30)::int AS month_from_registration,
+       (SELECT MAX(delivery_days) - MIN(delivery_days)
+        FROM sellers
+        WHERE category != 'Bedding'
+          AND seller_id IN (
+              SELECT seller_id
+              FROM sellers
+              WHERE category != 'Bedding'
+              GROUP BY seller_id
+              HAVING COUNT(DISTINCT category) >= 1 AND SUM(revenue) <= 50000
+          )
+       ) AS max_delivery_difference
 FROM sellers
 WHERE category != 'Bedding'
 GROUP BY seller_id
-HAVING COUNT(DISTINCT category) > 1 AND SUM(revenue) <= 50000
+HAVING COUNT(DISTINCT category) >= 1 AND SUM(revenue) <= 50000
 ORDER BY seller_id ASC;
+
 
 -- 2 Часть
 -- Пункт 3
